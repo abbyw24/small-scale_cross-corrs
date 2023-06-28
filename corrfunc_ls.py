8d26ade0f454b721be55dc0af0c2980bcc6e7ecc
Abby Williams <abby.e.williams@nyu.edu>
2023-06-28 15:41:23 -0500

    add cross-correlations

--- corrfunc_ls.py
+++ corrfunc_ls.py
@@ -3,7 +3,7 @@ import Corrfunc
 from Corrfunc.theory import DD, DDrppi
 
 
-def set_up_cf_data(data, randmult, rmin, rmax, nbins, logbins=True):
+def set_up_cf_data(data, randmult, rmin, rmax, nbins, logbins=True, dtype='float32'):
     """Helper function to set up data sets for Corrfunc."""
     if logbins:
         r_edges = np.logspace(np.log10(rmin), np.log10(rmax), nbins+1)
@@ -13,19 +13,21 @@ def set_up_cf_data(data, randmult, rmin, rmax, nbins, logbins=True):
     nd = len(data)
     boxsize = round(np.amax(data))
     nr = randmult * nd
-    rand_set = np.random.uniform(0, boxsize, (nr,3)).astype('float32')
-    return dict(r_edges=r_edges, r_avg=r_avg, nd=nd, boxsize=boxsize, nr=nr, rand_set=rand_set)
+    rand_set = np.random.uniform(0, boxsize, (nr,3)).astype(dtype)
+    data_set = data.astype(dtype)
+    return dict(r_edges=r_edges, r_avg=r_avg, nd=nd, boxsize=boxsize, nr=nr, rand_set=rand_set, data_set=data_set)
 
 
-def compute_3D_ls(data, randmult, rmin, rmax, nbins, logbins=True, periodic=True, nthreads=12, rr_fn=None, prints=False):
+# AUTO-CORRELATIONS #
+def compute_3D_ls_auto(data, randmult, rmin, rmax, nbins, logbins=True, periodic=True, nthreads=12, rr_fn=None, prints=False):
     """Estimate the 3D 2-pt. autocorrelation function."""
 
     # set up data
-    dataforcf = set_up_cf_data(data, randmult, rmin, rmax, nbins, logbins=logbins)
-    r_edges, r_avg, nd, boxsize, nr, rand_set = dataforcf.values()
+    dataforcf = set_up_cf_data(data, randmult, rmin, rmax, nbins, logbins=logbins, dtype='float32')
+    r_edges, r_avg, nd, boxsize, nr, rand_set, data_set = dataforcf.values()
 
     # unpack
-    x, y, z = data.T
+    x, y, z = data_set.T
     x_rand, y_rand, z_rand = rand_set.T
 
     dd_res = DD(1, nthreads, r_edges, x, y, z, boxsize=boxsize, periodic=periodic, output_ravg=True)
@@ -53,15 +55,15 @@ def compute_3D_ls(data, randmult, rmin, rmax, nbins, logbins=True, periodic=True
     return r_avg, results_xi
 
 
-def compute_2D_ls(data, randmult, rpmin, rpmax, nrpbins, pimax, logbins=True, periodic=True, nthreads=12, rr_fn=None, prints=False):
+def compute_2D_ls_auto(data, randmult, rpmin, rpmax, nrpbins, pimax, logbins=True, periodic=True, nthreads=12, rr_fn=None, prints=False):
     """Estimate the projected (2D) 2-pt. autocorrelation function."""
 
     # set up data
     dataforcf = set_up_cf_data(data, randmult, rpmin, rpmax, nrpbins, logbins=logbins)
-    rp_edges, rp_avg, nd, boxsize, nr, rand_set = dataforcf.values()
+    rp_edges, rp_avg, nd, boxsize, nr, rand_set, data_set = dataforcf.values()
 
     # unpack
-    x, y, z = data.T
+    x, y, z = data_set.T
     x_rand, y_rand, z_rand = rand_set.T
 
     dd_res = DDrppi(1, nthreads, pimax, rp_edges, x, y, z, boxsize=boxsize, periodic=periodic)
@@ -87,3 +89,44 @@ def compute_2D_ls(data, randmult, rpmin, rpmax, nrpbins, pimax, logbins=True, pe
         print("2d counts converted to cf")
     
     return rp_avg, results_wp
+
+
+# CROSS-CORRELATIONS #
+def compute_3D_ls_cross(data, tracers, randmult, rmin, rmax, nbins, logbins=True, periodic=True, nthreads=12, rr_fn=None, prints=False):
+    """Estimate the 3D 2-pt. cross-correlation function."""
+
+    # set up data: random set goes with tracers
+    dataforcf = set_up_cf_data(data, randmult, rmin, rmax, nbins, logbins=logbins, dtype='float32')
+    r_edges, r_avg, ndata, boxsized, _, _, data_set = dataforcf.values()
+    tracersforcf = set_up_cf_data(tracers, randmult, rmin, rmax, nbins, logbins=logbins, dtype='float32')
+    r_edges, r_avg, ntracers, boxsizet, nr, rand_set, tracer_set = tracersforcf.values()
+    assert boxsized==boxsizet, "data and tracer set must have the same boxsize!"
+
+    # unpack
+    xd, yd, zd = data_set.T
+    xt, yt, zt = tracer_set.T
+    x_rand, y_rand, z_rand = rand_set.T
+
+    dd_res = DD(0, nthreads, r_edges, xd, yd, zd, X2=xt, Y2=yt, Z2=zt, boxsize=boxsize, periodic=periodic, output_ravg=True)
+    if prints:
+        print("DD calculated")
+    dr_res = DD(0, nthreads, r_edges, xd, yd, zd, X2=x_rand, Y2=y_rand, Z2=z_rand, boxsize=boxsize, periodic=periodic)
+    if prints:
+        print("DR calculated")
+    
+    if rr_fn:
+        rr_res = np.load(rr_fn, allow_pickle=True)
+    else:
+        rr_res = DD(1, nthreads, r_edges, x_rand, y_rand, z_rand, boxsize=boxsize, periodic=periodic)
+    if prints:
+        print("RR calculated")
+
+    dd = np.array([x['npairs'] for x in dd_res], dtype=float)
+    dr = np.array([x['npairs'] for x in dr_res], dtype=float)
+    rr = np.array([x['npairs'] for x in rr_res], dtype=float)
+
+    results_xi = Corrfunc.utils.convert_3d_counts_to_cf(ndata, ntracers, nr, nr, dd, dr, dr, rr)
+    if prints:
+        print("3d counts converted to cf")
+
+    return r_avg, results_xi

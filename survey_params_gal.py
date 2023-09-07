53c868ec0fd4648d22c9d7613b7ff19a0b82b9df
Abby Williams <abby.e.williams@nyu.edu>
2023-09-06 23:08:27 -0500

    add SPHEREx and update DESI galaxy densities

--- survey_params_gal.py
+++ survey_params_gal.py
@@ -1,6 +1,7 @@
 """
-Copy from Yun-Ting Chen.
+Modified copy from Yun-Ting Chen.
 Class to get source number density as a function of redshift for SDSS and DESI spectroscopic surveys.
++ added analogous class for SPHEREx
 """
 
 import numpy as np
@@ -272,29 +273,57 @@ class DESI_param:
         
         # [dN/d(Mpc/h)^3]
         self.n_Mpc3 = self.n_zbin_deg2 / dV
-                
-            
-    def LRG_density_table(self):
+    
+
+    def ELG_data_density_table(self):
         '''
-        https://arxiv.org/pdf/1611.00036.pdf
-        table 2.3
-        
+        https://arxiv.org/pdf/2208.08513.pdf
+        Fig 19 top; blue line
         ns/zbin/deg^2 [deg^-2]
         '''
-        zbinedges = np.arange(0.6,2,0.1)
+        data = np.loadtxt('/scratch/08811/aew492/small-scale_cross-corrs/number_density/DESI_ELG_South-DECaLS.csv',
+                          delimiter=',')
+        z_data = data[:, 0]
+        N_data = data[:, 1]/0.05 # dN/dz/ddeg^2
+        zbinedges = np.linspace(z_data[0], z_data[-1], 500)
+        zbinedges = (zbinedges[1:] + zbinedges[:-1])/2
         zbins_min = zbinedges[:-1]
         zbins_max = zbinedges[1:]
-        zbins = (zbins_min + zbins_max) / 2        
+        zbins = (zbins_min + zbins_max) / 2 
         dz = np.diff(zbinedges)
-        
-        ns = np.zeros_like(zbins)
-        ns[:6] = [832, 986, 662, 272, 51, 17]
+
+        ns = np.interp(zbins, z_data, N_data)
         ns *= dz
-        
-        LRGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+
+        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
                    'ns': ns}
-        
-        return LRGtable
+
+        return ELGtable
+
+    def LRG_data_density_table(self):
+        '''
+        https://arxiv.org/pdf/2208.08515.pdf
+        Fig 1; gray hist
+        ns/zbin/deg^2 [deg^-2]
+        '''
+        data = np.loadtxt('/scratch/08811/aew492/small-scale_cross-corrs/number_density/DESI_LRG.csv',
+                          delimiter=',')
+        z_data = data[:, 0]
+        N_data = data[:, 1]/0.05 # dN/dz/ddeg^2
+        zbinedges = np.linspace(z_data[0], z_data[-1], 500)
+        zbinedges = (zbinedges[1:] + zbinedges[:-1])/2
+        zbins_min = zbinedges[:-1]
+        zbins_max = zbinedges[1:]
+        zbins = (zbins_min + zbins_max) / 2 
+        dz = np.diff(zbinedges)
+
+        ns = np.interp(zbins, z_data, N_data)
+        ns *= dz
+
+        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+                   'ns': ns}
+
+        return ELGtable
     
     def QSO_density_table(self):
         '''
@@ -317,28 +346,6 @@ class DESI_param:
                    'ns': ns}
         
         return QSOtable
-     
-    def ELG_density_table(self):
-        '''
-        https://arxiv.org/pdf/1611.00036.pdf
-        table 2.3
-        
-        ns/zbin/deg^2 [deg^-2]
-        '''
-        zbinedges = np.arange(0.6,2,0.1)
-        zbins_min = zbinedges[:-1]
-        zbins_max = zbinedges[1:]
-        zbins = (zbins_min + zbins_max) / 2        
-        dz = np.diff(zbinedges)
-        
-        ns = np.zeros_like(zbins)
-        ns[:-2] = [309,2269,1923,2094,1441,1353,1337,523,466,329,126]
-        ns *= dz
-        
-        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
-                   'ns': ns}
-        
-        return ELGtable
     
     def BGS_density_table(self):
         '''
@@ -383,3 +390,101 @@ class DESI_param:
                    'ns': ns}
         
         return LAFtable
+    
+
+class SPHEREx_param:
+    '''
+    public products repo at https://github.com/SPHEREx/Public-products
+
+    note unlike the other surveys, there's no distinction between different galaxy types.
+    also note that this one is stripped down because the number densities are provided in (h/Mpc)^3 \
+    and for my purposes this is the only unit I currently need.
+    but here we add the different redshift uncertainties sigma_z.
+    '''
+    def __init__(self, z=0, tracer_name='ELG', sigma_z=None):
+      
+        self.z = z
+        if sigma_z is None:
+            sigma_z = 0.01
+            print(f"defaulting to sigma_z={sigma_z}")
+        self.sigma_z = sigma_z
+        self._set_sigma_z_bin()
+        self.tracer_name = tracer_name
+        self.print_name = 'SPHEREx ' + tracer_name
+        self.print_name_survey = 'SPHEREx'
+        self._calc_params()
+    
+    def _calc_params(self):
+        self._get_density()
+        
+
+    def _get_density(self):
+        
+        table = self.galaxy_density_table()
+            
+        zbins_min, zbins_max = table['zbins_min'], table['zbins_max']
+        zidx = np.where((self.z >= zbins_min) & (self.z < zbins_max))[0]
+
+        if len(zidx) == 0:
+            self.n_zbin_deg2 = 0
+            self.n_z_deg2 = 0
+            self.n_z_sr = 0
+            self.n_Mpc3 = 0
+            return
+        
+        self.n_Mpc3 = table['ns'][zidx[0]]
+
+        self.bias = table['bs'][zidx[0]]
+    
+            
+    def galaxy_density_table(self):
+        '''
+        https://github.com/SPHEREx/Public-products/blob/master/galaxy_density_v28_base_cbe.txt
+
+        ns [dN/d(Mpc/h)^3]
+        '''
+        zbinedges = np.append(np.arange(0.0, 1.0, 0.2), np.arange(1.0, 5., 0.6))
+        zbins_min = zbinedges[:-1]
+        zbins_max = zbinedges[1:]
+        zbins = (zbins_min + zbins_max) / 2        
+        dz = np.diff(zbinedges)
+        
+        ns = np.zeros_like(zbins)
+        # number densities in each redshift bin for each redshift uncertainty sample
+        ns_all = np.array([
+            [0.00997, 0.00411, 0.000501, 7.05e-05, 3.16e-05, 1.64e-05, 3.59e-06, 8.07e-07, 1.84e-06, 1.5e-06, 1.13e-06],
+            [0.0123, 0.00856, 0.00282, 0.000937, 0.00043, 5e-05, 8.03e-06, 3.83e-06, 3.28e-06, 1.07e-06, 6.79e-07],
+            [0.0134, 0.00857, 0.00362, 0.00294, 0.00204, 0.000212, 6.97e-06, 2.02e-06, 1.43e-06, 1.93e-06, 6.79e-07],
+            [0.0229, 0.0129, 0.00535, 0.00495, 0.00415, 0.000796, 7.75e-05, 7.87e-06, 2.46e-06, 1.93e-06, 1.36e-06],
+            [0.0149, 0.00752, 0.00327, 0.0025, 0.00183, 0.000734, 0.000253, 5.41e-05, 2.99e-05, 9.41e-06, 2.04e-06]
+        ])
+        ns[:] = ns_all[self.sigma_z_idx]
+
+        bs = np.zeros_like(zbins)
+        # galaxy bias in each redshift bin for each redshift uncertainty sample
+        bs_all = np.array([
+            [1.3, 1.5, 1.8, 2.3, 2.1, 2.7, 3.6, 2.3, 3.2, 2.7, 3.8],
+            [1.2, 1.4, 1.6, 1.9, 2.3, 2.6, 3.4, 4.2, 4.3, 3.7, 4.6],
+            [1.0, 1.3, 1.5, 1.7, 1.9, 2.6, 3.0, 3.2, 3.5, 4.1, 5.0],
+            [0.98, 1.3, 1.4, 1.5, 1.7, 2.2, 3.6, 3.7, 2.7, 2.9, 5.0],
+            [0.83, 1.2, 1.3, 1.4, 1.6, 2.1, 3.2, 4.2, 4.1, 4.5, 5.0]
+        ])
+        bs[:] = bs_all[self.sigma_z_idx]
+        
+        table = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+                   'ns': ns, 'bs': bs}
+        
+        return table
+    
+
+    def _set_sigma_z_bin(self):
+        """
+        which of 5 redshift uncertainty bins are we in?
+        this determines the number densities and galaxy bias in each redshift bin.
+        """
+        sigma_zs = np.array([0., 0.003, 0.01, 0.03, 0.1, 0.2])
+        sigmazbins_min = sigma_zs[:-1]
+        sigmazbins_max = sigma_zs[1:]
+        sigmazbins = (sigmazbins_min + sigmazbins_max) / 2
+
+        self.sigma_z_idx = np.where((self.sigma_z > sigmazbins_min) & (self.sigma_z <= sigmazbins_max))[0]

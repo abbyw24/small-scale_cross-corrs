5999c9fd0f3c91141525efd1556f55c57b404e94
Abby Williams <aew492@login4.frontera.tacc.utexas.edu>
2024-12-09 15:01:59 -0600

    added HSC case to survey_params_gal

--- code/survey_params_gal.py
+++ code/survey_params_gal.py
@@ -5,8 +5,11 @@ Class to get source number density as a function of redshift for SDSS and DESI s
 """
 
 import numpy as np
+import os
 import astropy.units as u
+import astropy.cosmology.units as cu
 from astropy.cosmology import Planck15 as cosmo
+
 
 class eBOSS_param:
     '''
@@ -192,205 +195,47 @@ class eBOSS_param:
                    'ns': ns}
         
         return LRGtable    
-    
+
+
 class DESI_param:
     '''
-    https://arxiv.org/pdf/1611.00036.pdf
-    table 2.3 & 2.6
-    deltaz~5e-4 p.8
+    https://arxiv.org/pdf/2404.03000 
     '''
-    def __init__(self, z=0, tracer_name='LRG'):
-        
-    
-        self.tracer_names = ['BGS', 'LRG', 'ELG', 'QSO', 'LAF']        
+    def __init__(self, z=0, tracer_name='ELG'):
+
+        assert tracer_name in ['ELG', 'LRG', 'QSO'], "unknown input tracer_name"
+
         self.z = z
         self.tracer_name = tracer_name
         self.print_name = 'DESI ' + tracer_name
-        if tracer_name == 'LAF':
-            self.print_name = 'DESI QSO'
         self.print_name_survey = 'DESI'
         self._calc_params()
-    
+
     def _calc_params(self):
-        self._get_area()
         self._get_density()
-    
-    
-    def _get_area(self):
-        
-        Adeg = 14000
-        self.survey_area_deg = Adeg
-        
-        Asr = Adeg * (u.deg**2).to(u.sr)
-        self.survey_area_sr = Asr
-        
-        self.fsky = Asr / 4 / np.pi
-        self.lmin = np.pi / np.sqrt(Asr)
-        
-        return     
-        
+
     def _get_density(self):
-        
-        if self.tracer_name == 'LRG':
-            table = self.LRG_density_table()
-        elif self.tracer_name == 'QSO':
-            table = self.QSO_density_table()
-        elif self.tracer_name == 'ELG':
-            table = self.ELG_density_table()
-        elif self.tracer_name == 'BGS':
-            table = self.BGS_density_table()
-        elif self.tracer_name == 'LAF':
-            table = self.LAF_density_table()
-            
-        zbins_min, zbins_max = table['zbins_min'], table['zbins_max']
-        zidx = np.where((self.z >= zbins_min) & (self.z < zbins_max))[0]
 
-        if len(zidx) == 0:
-            self.n_zbin_deg2 = 0
-            self.n_z_deg2 = 0
-            self.n_z_sr = 0
-            self.n_Mpc3 = 0
-            return
-        
-        # [dN/d(deg^2)/(dzbin)]
-        self.n_zbin_deg2 = table['ns'][zidx[0]]
-        self.zbin_min = zbins_min[zidx[0]]
-        self.zbin_max = zbins_max[zidx[0]]
-        
-        # [dN/dz/d(deg^2)]
-        self.n_z_deg2 = self.n_zbin_deg2 / (self.zbin_max - self.zbin_min)
+        # get density info (z,n)
+        z, n = self.density_table()
 
-        # [dN/dz/dsr]
-        self.n_z_sr = self.n_zbin_deg2 * (u.deg**-2).to(u.sr**-1) \
-                        / (self.zbin_max - self.zbin_min)
-        
-        chi1 = cosmo.comoving_distance(self.zbin_max)
-        chi2 = cosmo.comoving_distance(self.zbin_min)
-        
-        dchi = (chi1 - chi2)
-        dA = (((chi1 + chi2) / 2) / (u.rad).to(u.deg))**2
-        dV = (dchi * dA).value * cosmo.h**3 # [(Mpc/h)**3]
-        
-        # [dN/d(Mpc/h)^3]
-        self.n_Mpc3 = self.n_zbin_deg2 / dV
-    
+        # closest data point to the input redshift
+        z_idx = np.argmin(np.abs(self.z - z))
 
-    def ELG_data_density_table(self):
-        '''
-        https://arxiv.org/pdf/2208.08513.pdf
-        Fig 19 top; blue line
-        ns/zbin/deg^2 [deg^-2]
-        '''
-        data = np.loadtxt('/scratch/08811/aew492/small-scale_cross-corrs/number_density/DESI_ELG_South-DECaLS.csv',
-                          delimiter=',')
-        z_data = data[:, 0]
-        N_data = data[:, 1]/0.05 # dN/dz/ddeg^2
-        zbinedges = np.linspace(z_data[0], z_data[-1], 500)
-        zbinedges = (zbinedges[1:] + zbinedges[:-1])/2
-        zbins_min = zbinedges[:-1]
-        zbins_max = zbinedges[1:]
-        zbins = (zbins_min + zbins_max) / 2 
-        dz = np.diff(zbinedges)
+        self.n_Mpc3 = n[z_idx]
 
-        ns = np.interp(zbins, z_data, N_data)
-        ns *= dz
+    def density_table(self):
 
-        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
-                   'ns': ns}
+        z, n = np.load(f'../data/DESI/DESI_{self.tracer_name}_density.npy').T
 
-        return ELGtable
+        # check that the input redshift is within the data range
+        assert (self.z >= min(z)) & (self.z <= max(z)), \
+            f"input redshift ({self.z}) outside density table range ({min(z):.2f}-{max(z):.2f})"
 
-    def LRG_data_density_table(self):
-        '''
-        https://arxiv.org/pdf/2208.08515.pdf
-        Fig 1; gray hist
-        ns/zbin/deg^2 [deg^-2]
-        '''
-        data = np.loadtxt('/scratch/08811/aew492/small-scale_cross-corrs/number_density/DESI_LRG.csv',
-                          delimiter=',')
-        z_data = data[:, 0]
-        N_data = data[:, 1]/0.05 # dN/dz/ddeg^2
-        zbinedges = np.linspace(z_data[0], z_data[-1], 500)
-        zbinedges = (zbinedges[1:] + zbinedges[:-1])/2
-        zbins_min = zbinedges[:-1]
-        zbins_max = zbinedges[1:]
-        zbins = (zbins_min + zbins_max) / 2 
-        dz = np.diff(zbinedges)
+        n *= 1e-4  # units in plot (Mpc/h)^3
 
-        ns = np.interp(zbins, z_data, N_data)
-        ns *= dz
+        return z, n
 
-        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
-                   'ns': ns}
-
-        return ELGtable
-    
-    def QSO_density_table(self):
-        '''
-        https://arxiv.org/pdf/1611.00036.pdf
-        table 2.3
-        
-        ns/zbin/deg^2 [deg^-2]
-        '''
-        zbinedges = np.arange(0.6,2,0.1)
-        zbins_min = zbinedges[:-1]
-        zbins_max = zbinedges[1:]
-        zbins = (zbins_min + zbins_max) / 2        
-        dz = np.diff(zbinedges)
-        
-        ns = np.zeros_like(zbins)
-        ns[:] = [47,55,61,67,72,76,80,83,85,87,87,87,86]
-        ns *= dz
-        
-        QSOtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
-                   'ns': ns}
-        
-        return QSOtable
-    
-    def BGS_density_table(self):
-        '''
-        https://arxiv.org/pdf/1611.00036.pdf
-        table 2.5
-        
-        ns/zbin/deg^2 [deg^-2]
-        '''
-        zbinedges = np.arange(0,0.6,0.1)
-        zbins_min = zbinedges[:-1]
-        zbins_max = zbinedges[1:]
-        zbins = (zbins_min + zbins_max) / 2        
-        dz = np.diff(zbinedges)
-        
-        ns = np.array([1165, 3074, 1909,732,120], dtype=float)
-        ns *= dz
-        
-        BGStable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
-                   'ns': ns}
-        
-        return BGStable
-
-    def LAF_density_table(self):
-        '''
-        https://arxiv.org/pdf/1611.00036.pdf
-        table 2.7
-        NOTE: these numbers are for the background QSO of LAF
-        ns/zbin/deg^2 [deg^-2]
-        '''
-        zbins = np.array([1.96,2.12,2.28,2.43,2.59,2.75,2.91,
-                          3.07,3.23,3.39,3.55, 3.70, 3.86, 4.02])
-        zbinedges = (zbins[1:] + zbins[:-1])/2
-        zbinedges = np.concatenate(([zbinedges[0]-0.16], zbinedges, [zbinedges[-1]+0.16]))
-        zbinedges[0] = 1.9
-        zbins_min = zbinedges[:-1]
-        zbins_max = zbinedges[1:]
-        dz = np.diff(zbinedges)
-        ns = np.array([82, 69, 53, 43, 37, 31, 26, 21, 16, 13, 9 ,7, 5, 3], dtype=float)
-        ns *= dz
-        
-        LAFtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
-                   'ns': ns}
-        
-        return LAFtable
-    
 
 class SPHEREx_param:
     '''
@@ -492,13 +337,16 @@ class SPHEREx_param:
 
 class HSC_param:
     '''
-    https://arxiv.org/pdf/2304.00701
+    HSC weak lensing tomographic inference: https://arxiv.org/pdf/2211.16516v2 
+    HSC Y3 results: http://arxiv.org/pdf/2304.00701 
+
 
     '''
 
-    def __init__(self, z=0):
+    def __init__(self, z=0, zbin=None):
 
         self.z = z
+        self.zbin = zbin
 
         self._calc_params()
 
@@ -508,19 +356,166 @@ class HSC_param:
 
     def _get_area(self):
 
-        self.survey_area_deg = 176 * (u.deg**2) # area of HSC Wide data in all 5 broadbands at full depth (Section 2.1)
+        self.survey_area_deg = 417 * (u.deg**2) # area of HSC Wide data in all 5 broadbands at full depth (Section 2.1)
         
-        self.survey_area_sr = Adeg.to(u.sr)
+        self.survey_area_sr = self.survey_area_deg.to(u.sr)
         
-        self.fsky = Asr / 4 / np.pi
+        self.fsky = self.survey_area_sr / (4 * np.pi)
 
     def _get_density(self):
 
         table = self.density_table()
+
+        z = table['z']
+        Pz = table['Pz']
+        self.dz = table['dz']
+        n = table['n'] << u.arcmin**(-2)
+        self.n_sr = n.to(1 / u.sr)
+
+        # total number of galaxies in this redshift bin
+        self.N = self.n_sr * self.survey_area_sr
+
+        zidx = np.argmin(np.abs(z[self.zbin] - self.z))
+        self.Pz = Pz[zidx]
+
+        # total number of galaxies at this redshift
+        self.N_z = self.N * self.Pz * self.dz
+        # print(f"total number of galaxies at redshift {self.z:.2f}: {self.N_z:.2f}")
+
+        # comoving volume element: thin shell from z to z+dz
+        chi1 = cosmo.comoving_distance(self.z)
+        chi2 = cosmo.comoving_distance(self.z + self.dz)
+        dchi = chi2 - chi1 # [Mpc]
+
+        # area of a sphere: A = r^2 * Omega -> A(z) = chi(z)^2 * A[sr]
+        A = chi1**2 * self.survey_area_sr.value # [Mpc^2]
+
+        # then the volume is
+        dV = dchi * A * (cosmo.h / cu.littleh)**3 # [(Mpc/h)^3]
+
+        # so the target number density is n(z) [(h/Mpc)^3] = n(z) [sr^(-1)] / dV
+        self.n_Mpc3 = self.N_z.value / dV
+        # print(f"target number density = {self.n_Mpc3.value:.3e}")
+    
+
+    def density_table(self):
+        """
+        If input zbin is `None`, determines the redshift bin based on which distribution has the highest P(z)
+        at the input redshift. Otherwise asserts that the input redshift falls within input zbin.
+
+        Returns
+        -------
+        table : dict of galaxy number densities as a function of redshift
+
+        """
+
+        if self.zbin is not None:
+            data = np.load(f'../data/HSC/HSC_dNdz_zbin{zbin}.npy', allow_pickle=True).item()
+            assert (self.z >= min(data['z'])) & (self.z <= max(data['z'])), \
+                f"input redshift ({self.z:.2f}) outside of zbin {self.zbin} range ({min(data['z']):.2f}-{max(data['z']):.2f})"
+
+            z_thisbin = data['z']
+            Pz_thisbin = data['pz']
         
+        else:
+            # load data for all bins
+            z = []
+            Pz = []
+            for zbin in range(0, 4):
+                data = np.load(f'../data/HSC/HSC_dNdz_zbin{zbin}.npy', allow_pickle=True).item()
+                z.append(data['z'])
+                Pz.append(data['pz'])
+
+            Pz_target = []
+            for zbin in range(0, 4):
+                if (self.z >= min(z[zbin])) & (self.z <= max(z[zbin])):
+                    idx = np.argmin(np.abs(z[zbin] - self.z))
+                    Pz_target.append(Pz[zbin][idx])
+                else:
+                    Pz_target.append(0)
+            assert sum(Pz_target) > 0., f"no P(z) found at this redshift ({self.z})"
+
+            # the redshift bin is the one with the highest P(z) at the input redshift:
+            self.zbin = np.argmax(Pz_target)
+
+            z_thisbin = z[self.zbin]
+            Pz_thisbin = Pz[self.zbin]
+
+        # effective galaxy number densities in each bin: from Section 3.1 of https://arxiv.org/pdf/2211.16516v2
+        n = [
+            3.77, 5.07, 4.00, 2.12
+        ] # per square arcmin
+
+        dz = np.diff(z_thisbin)
+        assert np.allclose(dz, dz[0]), "dz are not all equal"
+
+        table = { 'z' : z_thisbin, 'Pz' : Pz_thisbin, 'dz' : dz[0], 'n' : n[self.zbin] }
+
+        return table
+
+
+"""
+DEPRECATED DESI CLASS
+
+class DESI_param:
+    '''
+    https://arxiv.org/pdf/1611.00036.pdf
+    table 2.3 & 2.6
+    deltaz~5e-4 p.8
+    '''
+    def __init__(self, z=0, tracer_name='LRG'):
+        
+        self.data_dir = '/work2/08811/aew492/frontera/small-scale_cross-corrs/data/DESI'
+        self.tracer_names = ['BGS', 'LRG', 'ELG', 'QSO', 'LAF']        
+        self.z = z
+        self.tracer_name = tracer_name
+        self.print_name = 'DESI ' + tracer_name
+        if tracer_name == 'LAF':
+            self.print_name = 'DESI QSO'
+        self.print_name_survey = 'DESI'
+        self._calc_params()
+    
+    def _calc_params(self):
+        self._get_area()
+        self._get_density()
+    
+    
+    def _get_area(self):
+        
+        Adeg = 14000
+        self.survey_area_deg = Adeg
+        
+        Asr = Adeg * (u.deg**2).to(u.sr)
+        self.survey_area_sr = Asr
+        
+        self.fsky = Asr / 4 / np.pi
+        self.lmin = np.pi / np.sqrt(Asr)
+        
+        return     
+        
+    def _get_density(self):
+        
+        if self.tracer_name == 'LRG':
+            table = self.LRG_density_table()
+        elif self.tracer_name == 'QSO':
+            table = self.QSO_density_table()
+        elif self.tracer_name == 'ELG':
+            table = self.ELG_density_table()
+        elif self.tracer_name == 'BGS':
+            table = self.BGS_density_table()
+        elif self.tracer_name == 'LAF':
+            table = self.LAF_density_table()
+            
         zbins_min, zbins_max = table['zbins_min'], table['zbins_max']
         zidx = np.where((self.z >= zbins_min) & (self.z < zbins_max))[0]
 
+        if len(zidx) == 0:
+            self.n_zbin_deg2 = 0
+            self.n_z_deg2 = 0
+            self.n_z_sr = 0
+            self.n_Mpc3 = 0
+            return
+        
         # [dN/d(deg^2)/(dzbin)]
         self.n_zbin_deg2 = table['ns'][zidx[0]]
         self.zbin_min = zbins_min[zidx[0]]
@@ -542,19 +537,122 @@ class HSC_param:
         
         # [dN/d(Mpc/h)^3]
         self.n_Mpc3 = self.n_zbin_deg2 / dV
+    
 
-    def density_table(self):
-
-        data = np.load('../data/HSC_dNdz_colorcut.npy', allow_pickle=True).item()
-        zbinedges = data['zedges']
+    def ELG_density_table(self):
+        '''
+        https://arxiv.org/pdf/2208.08513.pdf
+        Fig 19 top; blue line
+        ns/zbin/deg^2 [deg^-2]
+        '''
+        data = np.loadtxt(os.path.join(self.data_dir, f'DESI_ELG_South-DECaLS.csv'),
+                          delimiter=',')
+        z_data = data[:, 0]
+        N_data = data[:, 1]/0.05 # dN/dz/ddeg^2
+        zbinedges = np.linspace(z_data[0], z_data[-1], 500)
+        zbinedges = (zbinedges[1:] + zbinedges[:-1])/2
         zbins_min = zbinedges[:-1]
         zbins_max = zbinedges[1:]
-        zbins = data['zbins']
-        N = data['N']
+        zbins = (zbins_min + zbins_max) / 2 
+        dz = np.diff(zbinedges)
 
-        dz = np.diff(zbins)
-        print(dz)
-        ns = N * dz
+        ns = np.interp(zbins, z_data, N_data)
+        ns *= dz
 
-        table = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zmins_max,
+        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
                    'ns': ns}
+
+        return ELGtable
+
+    def LRG_density_table(self):
+        '''
+        https://arxiv.org/pdf/2208.08515.pdf
+        Fig 1; gray hist
+        ns/zbin/deg^2 [deg^-2]
+        '''
+        data = np.loadtxt(os.path.join(self.data_dir, f'DESI_LRG.csv'),
+                          delimiter=',')
+        z_data = data[:, 0]
+        N_data = data[:, 1]/0.05 # dN/dz/ddeg^2
+        zbinedges = np.linspace(z_data[0], z_data[-1], 500)
+        zbinedges = (zbinedges[1:] + zbinedges[:-1])/2
+        zbins_min = zbinedges[:-1]
+        zbins_max = zbinedges[1:]
+        zbins = (zbins_min + zbins_max) / 2 
+        dz = np.diff(zbinedges)
+
+        ns = np.interp(zbins, z_data, N_data)
+        ns *= dz
+
+        ELGtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+                   'ns': ns}
+
+        return ELGtable
+    
+    def QSO_density_table(self):
+        '''
+        https://arxiv.org/pdf/1611.00036.pdf
+        table 2.3
+        
+        ns/zbin/deg^2 [deg^-2]
+        '''
+        zbinedges = np.arange(0.6,2,0.1)
+        zbins_min = zbinedges[:-1]
+        zbins_max = zbinedges[1:]
+        zbins = (zbins_min + zbins_max) / 2        
+        dz = np.diff(zbinedges)
+        
+        ns = np.zeros_like(zbins)
+        ns[:] = [47,55,61,67,72,76,80,83,85,87,87,87,86]
+        ns *= dz
+        
+        QSOtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+                   'ns': ns}
+        
+        return QSOtable
+    
+    def BGS_density_table(self):
+        '''
+        https://arxiv.org/pdf/1611.00036.pdf
+        table 2.5
+        
+        ns/zbin/deg^2 [deg^-2]
+        '''
+        zbinedges = np.arange(0,0.6,0.1)
+        zbins_min = zbinedges[:-1]
+        zbins_max = zbinedges[1:]
+        zbins = (zbins_min + zbins_max) / 2        
+        dz = np.diff(zbinedges)
+        
+        ns = np.array([1165, 3074, 1909,732,120], dtype=float)
+        ns *= dz
+        
+        BGStable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+                   'ns': ns}
+        
+        return BGStable
+
+    def LAF_density_table(self):
+        '''
+        https://arxiv.org/pdf/1611.00036.pdf
+        table 2.7
+        NOTE: these numbers are for the background QSO of LAF
+        ns/zbin/deg^2 [deg^-2]
+        '''
+        zbins = np.array([1.96,2.12,2.28,2.43,2.59,2.75,2.91,
+                          3.07,3.23,3.39,3.55, 3.70, 3.86, 4.02])
+        zbinedges = (zbins[1:] + zbins[:-1])/2
+        zbinedges = np.concatenate(([zbinedges[0]-0.16], zbinedges, [zbinedges[-1]+0.16]))
+        zbinedges[0] = 1.9
+        zbins_min = zbinedges[:-1]
+        zbins_max = zbinedges[1:]
+        dz = np.diff(zbinedges)
+        ns = np.array([82, 69, 53, 43, 37, 31, 26, 21, 16, 13, 9 ,7, 5, 3], dtype=float)
+        ns *= dz
+        
+        LAFtable = {'zbins': zbins, 'zbins_min': zbins_min, 'zbins_max': zbins_max,
+                   'ns': ns}
+        
+        return LAFtable
+
+"""
